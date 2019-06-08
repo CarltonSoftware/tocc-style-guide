9ebdc5002df7de8bb1c9c2ab2bd20758fca2d237
Alex <alex.wyett@gmail.com>
2019-06-08 18:11:27 +0100

    Update cdn

--- src/events.js
+++ src/events.js
@@ -23,4 +23,4 @@ export const UPDATE_ITEM_IN_STATE_COLLECTION_ERROR = 'UPDATE_ITEM_IN_STATE_COLLE
 
 export const DELETE_ITEM_FROM_STATE_COLLECTION = 'DELETE_ITEM_FROM_STATE_COLLECTION';
 
-export const CDN = "https://s3.eu-west-2.amazonaws.com/toccstyles/" + require('toccstyles').version;
+export const CDN = "https://toccstyles.s3.eu-west-2.amazonaws.com/" + require('toccstyles').version;
