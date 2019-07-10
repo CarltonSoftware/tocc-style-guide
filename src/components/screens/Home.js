f94b15e1f88d25705c27750a0bae20a53f2e68b5
Alex <alex.wyett@gmail.com>
2019-07-10 13:44:59 +0100

    Add colours to home page

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -7,17 +7,32 @@ class Home extends React.Component {
       <section className="hero">
         <div className="hero-body">
           <div className="container">
-            <h1 className="title">
-              The Original Cottage Company
-            </h1>
-            <h2 className="subtitle">
-              Style Guide
-            </h2>
-            <p>This site is intended to be a store of the components used in TOCC web applications.</p>
-            <p>
-              You will be able to browse components by type and see the 
-              marketing brand variances by selecting brand from the navigation bar above.
-            </p>
+            <div className="oc-content">
+              <h1>
+                The Original Cottage Company
+                <small className="oc-subtitle">Style Guide</small>
+              </h1>
+              <p>This site is intended to be a store of the components used in TOCC web applications.</p>
+              <p>
+                You will be able to browse components by type and see the 
+                marketing brand variances by selecting brand from the navigation bar above.
+              </p>
+            
+              <h4>Globals</h4>
+              <h5>Colours</h5>
+            </div>
+            <ul className="oc-list--inline">
+              { 
+                ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'darkgrey', 'lightgrey', 'reallylightgrey', 'almostwhite'].map((c, i) => {
+                  return (
+                    <li key={ i }>
+                      <span className={ "oc-label oc-label--" + c }>Color { c }</span>
+                    </li>
+                  );
+                })
+              }
+            </ul>
+            
           </div>
         </div>
       </section>
