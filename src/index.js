b09f7524b08f789d826e7c0a41943bd396b42d71
Alex <alex.wyett@gmail.com>
2019-07-18 12:03:14 +0100

    Add overlay example.

--- src/index.js
+++ src/index.js
@@ -15,10 +15,10 @@ client.getInstance().setInstance({
   apiRoot: 'https://toccl.api.tabs-software.co.uk',
   apiPrefix: '/v2',
   oAuthRedirectUrl: window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/oauth.html',
-  clientId: '16_2jgop2ihc0sg0sgwccskk0c0804k4ogw40s44ccgs4c8g0wgw4',
+  clientId: '16_2jgop2ihc0sg0sgwccskk0c0804k4ogw40s44ccgs4c8g0wgw4'
 });
 
-if (window.location.hash) {
+if (window.location.hash && window.location.hash.length > 30) {
   // synchronously save the token to localStorage
   client.getInstance().oAuthCallback(window.location.hash);
 
