ba6573eb0cbc0ec13b51834d8f48c419295fbf77
Alex <alex.wyett@gmail.com>
2020-07-16 10:30:21 +0100

    Handle localhost when using saml

--- src/index.js
+++ src/index.js
@@ -11,14 +11,19 @@ import Login from './components/Login';
 
 require('details-polyfill');
 
-client.getInstance().setInstance({
+var o = {
   apiRoot: 'https://toccl.api.tabs-software.co.uk',
   apiPrefix: '/v2',
   oAuthRedirectUrl: window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/oauth.html',
-  clientId: '16_2jgop2ihc0sg0sgwccskk0c0804k4ogw40s44ccgs4c8g0wgw4',
-  authPath: 'https://sso.originalcottages.co.uk/app/theoriginalcottagecompany_toccstyleguide_1/exk47j75qqzddtjAN357/sso/saml',
-  oAuthWithoutHost: true
-});
+  clientId: '16_2jgop2ihc0sg0sgwccskk0c0804k4ogw40s44ccgs4c8g0wgw4'
+};
+
+if (window.location.hostname !== 'localhost') {
+  o.authPath = 'https://sso.originalcottages.co.uk/app/theoriginalcottagecompany_toccstyleguide_1/exk47j75qqzddtjAN357/sso/saml';
+  o.oAuthWithoutHost = true;
+}
+
+client.getInstance().setInstance(o);
 
 if (window.location.hash && window.location.hash.length > 30) {
   // synchronously save the token to localStorage
