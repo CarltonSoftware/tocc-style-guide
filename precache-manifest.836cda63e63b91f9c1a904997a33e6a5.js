a119a735ae1b4ba5de028eb8781560267963e692
Alex <alex.wyett@gmail.com>
2020-11-16 14:27:05 +0000

    Updates

---
+++ precache-manifest.836cda63e63b91f9c1a904997a33e6a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b661a4a409629cad1f72",
+    "url": "/static/js/main.436720d0.chunk.js"
+  },
+  {
+    "revision": "1d8a78bf1a334d0d8eb8",
+    "url": "/static/js/2.ec6732cb.chunk.js"
+  },
+  {
+    "revision": "1d8a78bf1a334d0d8eb8",
+    "url": "/static/css/2.1a5a82d1.chunk.css"
+  },
+  {
+    "revision": "e528fb8e4055995417c38f848e755a75",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa39a9cb46b8f82d67b0866a4ff87940.js"
+  "/precache-manifest.836cda63e63b91f9c1a904997a33e6a5.js"
 );
 
 workbox.clientsClaim();
